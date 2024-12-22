41a92080f56617dfcab08ad89976007bd27cd753
21522604 <21522604@gm.uit.edu.vn>
2024-12-22 16:10:26 +0700

    Update Form "QLSach" Again

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace qltv
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            //Application.Run(new frmQLSach());
             Application.Run(new frmDangNhap());
         }
     }
