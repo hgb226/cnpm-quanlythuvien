2972b4b1436fc791a5132855c6a420094a3f3794
Bao <21520604@gm.uit.edu.vn>
2024-12-26 20:12:14 +0700

    Disable 'hide' when show other forms

--- frmGiaoDienChinh.cs
+++ frmGiaoDienChinh.cs
@@ -13,42 +13,36 @@ namespace qltv
         {
             frmQLSach QLSach = new frmQLSach();
             QLSach.Show();
-            this.Hide();
         }
 
         private void quánLýĐộcGiảToolStripMenuItem_Click(object sender, EventArgs e)
         {
             frmQLDocGia QLDocGia  = new frmQLDocGia();
             QLDocGia.Show();
-            this.Hide();
         }
 
         private void quảnLýMượnTrảToolStripMenuItem_Click(object sender, EventArgs e)
         {
             frmQLMuonTra QLMuonTra  = new frmQLMuonTra();
             QLMuonTra.Show();
-            this.Hide();
         }
 
         private void phiếuThuTiềnPhạtToolStripMenuItem_Click(object sender, EventArgs e)
         {
             frmPhieuthutienphat TienPhat = new frmPhieuthutienphat();
             TienPhat.Show();
-            this.Hide();
         }
 
         private void báoCáoThốngKêToolStripMenuItem_Click(object sender, EventArgs e)
         {
             frmBaoCaoThongKe BaoCao = new frmBaoCaoThongKe();
             BaoCao.Show();
-            this.Hide();
         }
 
         private void đổiMậtKhẩuToolStripMenuItem_Click(object sender, EventArgs e)
         {
             frmDoiMatKhau MatKhau = new frmDoiMatKhau();
             MatKhau.Show();
-            this.Hide();
         }
 
         private void thoátToolStripMenuItem_Click(object sender, EventArgs e)
